742b8f49e41e7a8d57da60c058760d1ee0f5c677
Alex E.T. Snyder <alexetsnyder@gmail.com>
2025-09-14 12:52:17 -0400

    Checks for All Dice Combos

--- DiceFishing/scenes/Dice.cs
+++ DiceFishing/scenes/Dice.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 public partial class Dice
 {
@@ -17,7 +16,7 @@ public partial class Dice
     public enum DiceScoreCombo
     {
         None = 0,
-        TwoOfAKind = 1,
+        Pair = 1,
         TwoPair = 2,
         ThreeOfAKind = 3,
         Straight = 4,
@@ -29,26 +28,208 @@ public partial class Dice
     public static DiceScoreCombo Evaluate(List<DiceType> dice)
     {
         dice.Sort();
-        //Regex fiveOfAKind = new Regex(@"[1-6]{5,}");
-        //Regex fourOfAKind = new Regex(@"[1-6]{4,}");
-        //Regex threeOfAKind = new Regex(@"[1-6]{3}");
-        Regex twoOfAKind = new Regex(@"([1-6]+)(?=\1{1})");
-
-        if (twoOfAKind.IsMatch(GetDiceString(dice)))
+        
+        if (IsFiveOfAKind(dice))
         {
-            return DiceScoreCombo.TwoOfAKind;
+            return DiceScoreCombo.FiveOfAKind;
         }
-
-        return DiceScoreCombo.None;
+        else if (IsFourOfAKind(dice))
+        {
+            return DiceScoreCombo.FourOfAKind;
+        }
+        else if (IsFullHouse(dice))
+        {
+            return DiceScoreCombo.FullHouse;
+        }
+        else if (IsStraight(dice))
+        {
+            return DiceScoreCombo.Straight;
+        }
+        else if (IsThreeOfAKind(dice))
+        {
+            return DiceScoreCombo.ThreeOfAKind;
+        }
+        else if (IsTwoPair(dice))
+        {
+            return DiceScoreCombo.TwoPair;
+        }
+        else if (IsPair(dice))
+        {
+            return DiceScoreCombo.Pair;
+        }
+        else
+        {
+            return DiceScoreCombo.None;
+        }
     }
 
-    private static string GetDiceString(List<DiceType> dice)
+    public static bool IsFiveOfAKind(List<DiceType> dice)
     {
-        string diceStr = "";
-        foreach (var die in dice)
+        for (int i = 0; i < dice.Count - 4; i++)
         {
-            diceStr += (int)die;
+            int match = 1;
+            for (int j = i + 1; j < dice.Count; j++)
+            {
+                if (dice[i] == dice[j])
+                {
+                    match++;
+                }
+            }
+
+            if (match == 5)
+            {
+                return true;
+            }
         }
-        return diceStr;
+
+        return false;
     }
+
+    public static bool IsFourOfAKind(List<DiceType> dice)
+    {
+        for (int i = 0; i < dice.Count - 3; i++)
+        {
+            int match = 1;
+            for (int j = i + 1; j < dice.Count; j++)
+            {
+                if (dice[i] == dice[j])
+                {
+                    match++;
+                }
+            }
+
+            if (match == 4)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool IsFullHouse(List<DiceType> dice)
+    {
+        var threeOfAKindValue = DiceType.None;
+
+        for (int i = 0; i < dice.Count - 2; i++)
+        {
+            for (int j = i + 1; j < dice.Count; j++)
+            {
+                for (int k = j + 1; k < dice.Count; k++)
+                {
+                    if (dice[i] == dice[j] && dice[j] == dice[k])
+                    {
+                        threeOfAKindValue = dice[i];
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (threeOfAKindValue != DiceType.None)
+        {
+            for (int i = 0; i < dice.Count; i++)
+            {
+                for (int j = i + 1; j < dice.Count; j++)
+                {
+                    if (threeOfAKindValue != dice[i] && dice[i] == dice[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static bool IsStraight(List<DiceType> dice)
+    {
+        if (dice.Count > 0)
+        {
+            var firstValue = dice[0];
+            for (int i = 1; i < dice.Count; i++)
+            {
+                if (firstValue != dice[i] - i)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool IsThreeOfAKind(List<DiceType> dice)
+    {
+        for (int i = 0; i < dice.Count - 2; i++)
+        {
+            int match = 1;
+            for (int j = i + 1; j < dice.Count;  j++)
+            {
+                if (dice[i] == dice[j])
+                {
+                    match++;
+                }
+            }
+
+            if (match == 3)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool IsTwoPair(List<DiceType> dice)
+    {
+        var firstPair = DiceType.None;
+
+        for (int i = 0; i < dice.Count; i++)
+        {
+            for (int j = i + 1; j < dice.Count; j++)
+            {
+                if (dice[i] == dice[j])
+                {
+                    firstPair = dice[i];
+                    break;
+                }
+            }
+        }
+
+        if (firstPair != DiceType.None)
+        {
+            for (int i = 0; i < dice.Count; i++)
+            {
+                for (int j = i + 1; j < dice.Count; j++)
+                {
+                    if (firstPair != dice[i] && dice[i] == dice[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static bool IsPair(List<DiceType> dice)
+    {
+        for (int i = 0; i < dice.Count; i++)
+        {
+            for (int j = i + 1; j < dice.Count; j++)
+            {
+                if (dice[i] == dice[j])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }  
 }
